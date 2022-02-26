bc72fb257c7ba0037964f7f358ceb18ec033e03b
Vipul Nikam <56763988+iam41@users.noreply.github.com>
2022-02-26 17:59:08 +0530

    Delete Lab 2 directory

--- Lab 2/clean.sh
+++
@@ -1,4 +0,0 @@
-test -f test-toggle && rm test-toggle
-test -f test-zem && rm test-zem
-
-echo "All the unwanted files were removed."
--- Lab 2/zemaphore.h
+++
@@ -1,8 +0,0 @@
-#include <pthread.h>
-
-typedef struct zemaphore {
-} zem_t;
-
-void zem_init(zem_t *, int);
-void zem_up(zem_t *);
-void zem_down(zem_t *);
